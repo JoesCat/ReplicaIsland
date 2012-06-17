cbaad8f04983b13ced550357ab331431bd650be7
Brian Carlstrom <bdc@google.com>
2012-06-16 23:33:00 -0700

    APKs part of CTS should have LOCAL_DEX_PREOPT := false (2 of 2)
    Change-Id: I4d1518c931488e5c8225e5667453090b0723510e

--- Android.mk
+++ Android.mk
@@ -20,6 +20,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 # and when built explicitly put it in the data partition
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
+# and because it is in data, do not strip classes.dex
+LOCAL_DEX_PREOPT := false
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
